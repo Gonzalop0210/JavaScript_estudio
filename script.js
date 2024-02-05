27de2be9658c4e4eeff7734fcc62a2808961f36f
Gonzalo Palma <gonzalopalmadev0210@gmail.com>
2024-02-05 12:15:29 -0500

    avanzando con los ejercicios

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let arraynumero = [];
-let numero = 0;
-do {
-    numero = parseFloat(prompt('Escribe el numero'));
-    if (!isNaN(numero) && numero >= 0) {
-        arraynumero.push(numero)
+const input_nombre = document.querySelector('.input__nombre')
+const input_apellido = document.querySelector('.input__apellido')
+const input_correo = document.querySelector('.input__correo')
+const boton = document.querySelector('.input__boton')
+const span_nombre = document.querySelector('.nombre')
+const span_apellido = document.querySelector('.apellido')
+const span_correo = document.querySelector('.correo')
+
+boton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (input_nombre.value === '' || input_apellido.value === '' || input_correo.value === '') {
+        alert('Llena todos los campos')
+        input_nombre.value = '';
+        input_apellido.value = '';
+        input_correo.value = '';
+        input_nombre.focus();
+    } else {
+        span_nombre.textContent = input_nombre.value;
+        span_apellido.textContent = input_apellido.value;
+        span_correo.textContent = input_correo.value;
     }
-} while (!isNaN(numero) && numero >= 0);
-
-console.log(arraynumero);
-
-let sumaArray = arraynumero.reduce((a, va) => a + va, 0);
-let media = sumaArray / arraynumero.length
-console.log(sumaArray);
-console.log(media);
+})
